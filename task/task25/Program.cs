b1a3d14bf030777b805c5f2b86b0e5faf0b45728
Dushichev <108294464+Dushichev@users.noreply.github.com>
2022-08-05 19:44:12 +0300

    выполнил дз

--- task/task25/Program.cs
+++ task/task25/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+ и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4-> 16*/
+
+Console.WriteLine("введите число A");
+int numA = int.Parse(Console.ReadLine());
+Console.WriteLine("введите число B");
+int numB = int.Parse(Console.ReadLine());
+
+/*double result = Math.Pow(a, b);
+Console.WriteLine(result);*/
+
+int result = 1;
+for (int i = 0; i < numB; i++)
+{
+    result = result * numA;
+}
+
+Console.WriteLine(result);
--- task/task29/Program.cs
+++ task/task29/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33-> [6, 1, 33]*/
+
+
+/*int[] myArray = new int[8];
+Random random = new Random();
+
+
+for (int i = 0; i < myArray.Length; i++)
+{
+    myArray[i] = random.Next(8);
+
+    Console.Write(myArray[i] + " ");
+}*/
+
+Console.WriteLine("Введите длинну массива");
+
+int elementArray = int.Parse(Console.ReadLine());
+
+int[] myArray = new int[elementArray];
+
+
+for (int i = 0; i < myArray.Length; i++)
+{
+    Console.Write("Введите элемент массива" + " ");
+    myArray[i] = int.Parse(Console.ReadLine());
+
+
+}
+Console.WriteLine("Массив");
+
+for (int i = 0; i < myArray.Length; i++)
+{
+    Console.Write(myArray[i] + " ");
+}
+
